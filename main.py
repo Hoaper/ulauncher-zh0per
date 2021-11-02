9839b3dc85e608c48b8e40b923647da4b4bea969
Hoaper <beeboplay@gmail.com>
2021-11-02 18:54:32 +0600

    test

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class KeywordListener(EventListener):
             icon='images/icon.png',
             name='Execute',
             description=event.get_argument(),
-            on_enter=OpenAction('/bin/gnome-terminal --title TERMINAL') ))
+            on_enter=OpenAction('/bin/bash') ))
 
         return RenderResultListAction(items)
 
